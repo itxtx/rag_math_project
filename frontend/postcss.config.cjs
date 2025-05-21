7d987614533f4220c3f28f34a6b9b528a17b4e44
itxtx <68209227+itxtx@users.noreply.github.com>
2025-05-21 14:23:09 +0200

    Revert "break"
    This reverts commit bb37de2e9011020852897f88f6d7f3a5c83f2c75.

--- frontend/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-// postcss.config.cjs
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {}, // This is for Tailwind v4
-    autoprefixer: {}, // Essential for adding vendor prefixes
-  },
-};
-
-module.exports = config;
